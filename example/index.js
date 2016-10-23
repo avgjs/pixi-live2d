5a61a9dc0bb91e867ac0bfa603c7f6d9db2b337a
Icemic <bingfeng.jw@gmail.com>
2016-10-23 22:27:21 +0800

    update example/index.js

--- example/index.js
+++ example/index.js
@@ -1,23 +1,9 @@
 import PIXI from 'pixi.js';
-import { Live2D } from '../src/lib/live2d';
-import Live2DSprite from '../src/index';
-// import Live2DSprite from '../src/Live2DSprite';
-
+import '../src/index';
 
 const renderer = new PIXI.WebGLRenderer(800, 600);
 document.getElementById('app').appendChild(renderer.view);
 const stage = new PIXI.Container();
-
-
-
-const modelEpsilon = {
-  'type':'Live2D Model Setting',
-  'name':'Epsilon2.1',
-  'model':'assets/Epsilon2.1/Epsilon2.1.moc',
-  'textures':[
-    'assets/Epsilon2.1/Epsilon2.1.2048/texture_00.png',
-  ]
-};
 
 const modelHaru = {
   "type":"Live2D Model Setting",
@@ -44,7 +30,7 @@ const modelHaru = {
   ],
   "layout":
   {
-    "center_x": 0.8,
+    "center_x": 0,
     "y": 1.2,
     "width": 2.9
   },
@@ -87,50 +73,46 @@ const modelHaru = {
 };
 
 
-// sprite.x = 800;
+
 const sprite = new PIXI.Sprite.fromImage('./7_room2_a.jpg');
 stage.addChild(sprite);
-// const live2dSprite2 = new Live2DSprite(modelEpsilon, renderer.view);
-// stage.addChild(live2dSprite2);
-const live2dSprite = new Live2DSprite(modelHaru, {
+
+// setTimeout(() => {
+//   const sprite2 = new PIXI.Sprite.fromImage('./pixiv4.jpg');
+//   sprite2.y = 350;
+//   stage.addChildAt(sprite2, 1);
+// }, 1000)
+
+const live2dSprite = new PIXI.Live2DSprite(modelHaru, {
   debugLog: false,
   randomMotion: false,
   eyeBlink: false,
   // audioPlayer: (...args) => console.log(...args)
 });
 stage.addChild(live2dSprite);
+
+live2dSprite.x = -105;
+// live2dSprite.y = -150;
+live2dSprite.adjustScale(0,0,0.7);
+live2dSprite.adjustTranslate(0.4, 0);
 live2dSprite.startRandomMotion('idle');
-// live2dSprite.startMotion('pinch_out', 0);
-live2dSprite.playSound('111111111111.mp3', 'sound/');
-// live2dSprite.setLipSync(0);
-const sprite2 = new PIXI.Sprite.fromImage('./pixiv4.jpg');
-sprite2.y = 550;
-// live2dSprite.interactive = true;
-// live2dSprite.buttonMode = true;
-// live2dSprite.on('click', () => console.log('click'))
-// stage.addChild(sprite2);
 
-// let t = null;
-//
-// setInterval(() => {
-//     t && t.destroy({ baseTexture: true });
-//     const texture = PIXI.RenderTexture.create(800, 600);
-//     renderer.render(sprite, texture, false);
-//     t = texture;
-// }, 16)
-
-// setTimeout(() => stage.removeChild(sprite), 3000)
-
-/* live2d */
-
-
-
-
-/* live2d */
+live2dSprite.on('click', (evt) => {
+  const point = evt.data.global;
+  if (live2dSprite.hitTest('body', point.x, point.y)) {
+    live2dSprite.startRandomMotionOnce('tap_body');
+  }
+  if (live2dSprite.hitTest('head', point.x, point.y)) {
+    live2dSprite.playSound('星のカケラ.mp3', 'sound/');
+  }
+});
+live2dSprite.on('mousemove', (evt) => {
+  const point = evt.data.global;
+  live2dSprite.setViewPoint(point.x, point.y);
+});
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(stage);
 }
-// window.animate = animate;
 animate();
