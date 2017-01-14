ef7684d50b968b1db6c11a50ad66551dc7daed5a
Icemic <bingfeng.jw@gmail.com>
2017-01-14 16:09:05 +0800

    Refactoring `MatrixStack` using glMatrix-mat4, close #3

--- src/lib/MatrixStack.js
+++ src/lib/MatrixStack.js
@@ -4,9 +4,14 @@
  *  only for the development of application related Live2D.
  *
  *  (c) Live2D Inc. All rights reserved.
+ *  (c) Modified by Icemic <bingfeng.web@gmail.com>
  */
 
-export default function MatrixStack() {}
+const mat4 = require('./glMatrix-mat4');
+
+const MatrixStack = {};
+
+export default MatrixStack;
 
 
 MatrixStack.matrixStack = [
@@ -41,28 +46,35 @@ MatrixStack.reset = function()
 
 MatrixStack.loadIdentity = function()
 {
-    for (var i = 0; i < 16; i++)
-    {
-        this.currentMatrix[i] = (i % 5 == 0) ? 1 : 0;
-    }
+    // for (var i = 0; i < 16; i++)
+    // {
+    //     this.currentMatrix[i] = (i % 5 == 0) ? 1 : 0;
+    // }
+    this.currentMatrix = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ];
 }
 
 
 
 MatrixStack.push = function()
 {
-    var offset = this.depth * 16;
-    var nextOffset = (this.depth + 1) * 16;
+    // var offset = this.depth * 16;
+    // var nextOffset = (this.depth + 1) * 16;
 
-    if (this.matrixStack.length < nextOffset + 16)
-    {
-        this.matrixStack.length = nextOffset + 16;
-    }
+    // if (this.matrixStack.length < nextOffset + 16)
+    // {
+    //     this.matrixStack.length = nextOffset + 16;
+    // }
 
-    for (var i = 0; i < 16; i++)
-    {
-        this.matrixStack[nextOffset + i] = this.currentMatrix[i];
-    }
+    // for (var i = 0; i < 16; i++)
+    // {
+    //     this.matrixStack[nextOffset + i] = this.currentMatrix[i];
+    // }
+    this.matrixStack.push(this.currentMatrix);
 
     this.depth++;
 }
@@ -78,11 +90,12 @@ MatrixStack.pop = function()
         this.depth = 0;
     }
 
-    var offset = this.depth * 16;
-    for (var i = 0; i < 16; i++)
-    {
-        this.currentMatrix[i] = this.matrixStack[offset + i];
-    }
+    // var offset = this.depth * 16;
+    // for (var i = 0; i < 16; i++)
+    // {
+    //     this.currentMatrix[i] = this.matrixStack[offset + i];
+    // }
+    this.currentMatrix = this.matrixStack.pop();
 }
 
 
@@ -96,25 +109,26 @@ MatrixStack.getMatrix = function()
 
 MatrixStack.multMatrix = function(matNew)
 {
-    var i, j, k;
+    // var i, j, k;
 
-    for (i = 0; i < 16; i++)
-    {
-        this.tmp[i] = 0;
-    }
+    // for (i = 0; i < 16; i++)
+    // {
+    //     this.tmp[i] = 0;
+    // }
 
-    for (i = 0; i < 4; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            for (k = 0; k < 4; k++)
-            {
-                this.tmp[i + j * 4] += this.currentMatrix[i + k * 4] * matNew[k + j * 4];
-            }
-        }
-    }
-    for (i = 0; i < 16; i++)
-    {
-        this.currentMatrix[i] = this.tmp[i];
-    }
+    // for (i = 0; i < 4; i++)
+    // {
+    //     for (j = 0; j < 4; j++)
+    //     {
+    //         for (k = 0; k < 4; k++)
+    //         {
+    //             this.tmp[i + j * 4] += this.currentMatrix[i + k * 4] * matNew[k + j * 4];
+    //         }
+    //     }
+    // }
+    // for (i = 0; i < 16; i++)
+    // {
+    //     this.currentMatrix[i] = this.tmp[i];
+    // }
+    mat4.multiply(this.currentMatrix, this.currentMatrix, matNew);
 }
